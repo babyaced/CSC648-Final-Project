8cb622794aa4b93375d1db06e29cb3e0fa0b5a9f
babyaced <dannyr.simpson@gmail.com>
2021-05-30 12:01:26 -0700

    started on footer responsiveness

--- application/frontend/src/components/Footer/Footer.js
+++ application/frontend/src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
 import {useState} from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 import TermsAndConditions from '../../components/Modals/TermsAndConditions'
 import PrivacyPolicy from "../Modals/PrivacyPolicy";
+
+import FacebookIcon from '../../images/Third Party Icons/icons8-facebook.svg'
+import InstagramIcon from '../../images/Third Party Icons/icons8-instagram.svg'
+import TwitterIcon from '../../images/Third Party Icons/icons8-twitter.svg'
+import EmailIcon from '../../images/Created Icons/MessagesBlack.svg'
 
 function Footer() {
   const [termsAndConditionsDisplay,setTermsAndConditionsDisplay]= useState(false);
@@ -28,29 +33,29 @@ function Footer() {
 
   return (
     <>
-    <div className={styles["footer"]}>
-      <div className={styles["our-team-footer"]}>Our Team<br />
-        <NavLink className={styles["nav-link"]} to="/Edgar" >Edgar</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Daniel" >Daniel</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Em" >Em</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Sabrina" >Sabrina</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Wenjie" >Wenjie</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Cameron" > Cameron</NavLink>
-        <NavLink className={styles["nav-link"]} to="/Wameedh" > Wameedh</NavLink>
+    <footer className={styles["footer"]}>
+      <div className={styles["our-team"]}>
+        <Link to="/Edgar" >Edgar</Link>
+        <Link to="/Daniel" >Daniel</Link>
+        <Link to="/Em" >Em</Link>
+        <Link to="/Sabrina" >Sabrina</Link>
+        <Link to="/Wenjie" >Wenjie</Link>
+        <Link to="/Cameron" > Cameron</Link>
+        <Link to="/Wameedh" > Wameedh</Link>
       </div>
-      <div className={styles['logos']}>
-        <a href="https://www.linkedin.com/"><i className="fa fa-facebook"></i></a>
-        <a href="https://www.instagram.com/"><i className="fa fa-instagram"></i></a>
-        <a href="https://twitter.com/"><i className="fa fa-twitter"></i></a>
-        <a href=""><i className="fa fa-envelope"></i></a>
+      <div className={styles['social-media-links']}>
+        <a href="https://www.facebook.com/"><img className={styles['facebook-icon']} src={FacebookIcon}/></a>
+        <a href="https://www.instagram.com/"><img className={styles['instagram-icon']} src={InstagramIcon}/></a>
+        <a href="https://twitter.com/"><img className={styles['twitter-icon']} src={TwitterIcon}/></a>
+        <a href=""><img className={styles['email-icon']} src={EmailIcon}/></a>
       </div>
+      <div className={styles['icons8-attribution']}>Icons by <a href="https://icons8.com/">icons8.com</a></div>
       <div className={styles['terms-conditions']}>
         <button className={styles['terms-button']} onClick={openTermsAndConditionsModal}>Terms of Use</button>
         |
         <button className={styles['policy-button']} onClick={openPrivacyPolicyModal}>Privacy Policy</button>
       </div>
-      <div className={styles['icons-8-attribution']}>Icons by <a href="https://icons8.com/">icons8.com</a></div>
-    </div>
+    </footer>
     {/* Modals */}
     <TermsAndConditions display={termsAndConditionsDisplay} onClose={closeTermsAndConditionsModal}/>
     <PrivacyPolicy display={privacyPolicyDisplay} onClose={closePrivacyPolicyModal}/>
