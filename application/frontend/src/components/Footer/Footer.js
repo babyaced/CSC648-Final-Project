32d8d2eed4931441ac0b55139dad538d973c1116
babyaced <dannyr.simpson@gmail.com>
2021-05-30 14:06:36 -0700

    responsive footer

--- application/frontend/src/components/Footer/Footer.js
+++ application/frontend/src/components/Footer/Footer.js
@@ -34,23 +34,28 @@ function Footer() {
   return (
     <>
     <footer className={styles["footer"]}>
-      <span className={styles["our-team-header"]}>Our Team</span>
-      <div className={styles["our-team"]}>
-        <Link to="/Edgar" >Edgar | </Link>
-        <Link to="/Daniel" >Daniel | </Link>
-        <Link to="/Em" >Em | </Link>
-        <Link to="/Sabrina" >Sabrina | </Link>
-        <Link to="/Wenjie" >Wenjie | </Link>
-        <Link to="/Cameron" >Cameron | </Link>
-        <Link to="/Wameedh" >Wameedh</Link>
+      <div className={styles["links"]}>
+        <span className={styles["our-team-header"]}><h5>Our Team</h5></span>
+        <div className={styles["our-team"]}>
+          <Link to="/Edgar" >Edgar | </Link>
+          <Link to="/Daniel" >Daniel | </Link>
+          <Link to="/Em" >Em | </Link>
+          <Link to="/Sabrina" >Sabrina | </Link>
+          <Link to="/Wenjie" >Wenjie | </Link>
+          <Link to="/Cameron" >Cameron | </Link>
+          <Link to="/Wameedh" >Wameedh</Link>
+        </div>
+        <span className={styles["social-media-header"]}><h5>Follow Us</h5></span>
+        <div className={styles['social-media-links']}>
+          <a href="https://www.facebook.com/"><img className={styles['facebook-icon']} src={FacebookIcon}/></a>
+          <a href="https://www.instagram.com/"><img className={styles['instagram-icon']} src={InstagramIcon}/></a>
+          <a href="https://twitter.com/"><img className={styles['twitter-icon']} src={TwitterIcon}/></a>
+          <a href=""><img className={styles['email-icon']} src={EmailIcon}/></a>
+        </div>
+        <span className={styles["icons8-attribution-header"]}><h5>Icons by</h5></span>
+        <div className={styles['icons8-attribution']}><a href="https://icons8.com/">icons8.com</a></div>
       </div>
-      <div className={styles['social-media-links']}>
-        <a href="https://www.facebook.com/"><img className={styles['facebook-icon']} src={FacebookIcon}/></a>
-        <a href="https://www.instagram.com/"><img className={styles['instagram-icon']} src={InstagramIcon}/></a>
-        <a href="https://twitter.com/"><img className={styles['twitter-icon']} src={TwitterIcon}/></a>
-        <a href=""><img className={styles['email-icon']} src={EmailIcon}/></a>
-      </div>
-      <div className={styles['icons8-attribution']}>Icons by <a href="https://icons8.com/">icons8.com</a></div>
+      
       <div className={styles['terms-conditions']}>
         <button className={styles['terms-button']} onClick={openTermsAndConditionsModal}>Terms of Use</button>
         <span>|</span>
