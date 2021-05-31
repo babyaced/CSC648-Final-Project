e3e1c0d2b3a97a9080289138c5dbcf03740a88c1
babyaced <dannyr.simpson@gmail.com>
2021-05-30 20:06:04 -0700

    message cards responsive

--- application/frontend/src/components/Cards/MessageCard/MessageCard.js
+++ application/frontend/src/components/Cards/MessageCard/MessageCard.js
@@ -6,11 +6,11 @@ import styles from "./MessageCard.module.css";
 
 function MessageCard({message, viewModal}) {
     return (
-        <div key={message.message_id} className={styles["messages-container-message"]} onClick={() => viewModal()}>
-            <img className={styles["messages-container-message-pic"]} src={message.profile_pic_link}/>
-            <div className={styles["messages-container-message-subject"]}>{message.subject}</div>
-            <div className={styles["messages-container-message-timestamp"]}>{new Date(message.timestamp).toLocaleString()}</div>
-            <div className={styles["messages-container-message-sender"]}>{message.display_name}</div>
+        <div key={message.message_id} className={styles["message"]} onClick={() => viewModal()}>
+            <img className={styles["message-pic"]} src={message.profile_pic_link}/>
+            <div className={styles["message-subject"]}>{message.subject}</div>
+            <div className={styles["message-timestamp"]}>{new Date(message.timestamp).toLocaleString()}</div>
+            <div className={styles["message-sender"]}>{message.display_name}</div>
         </div>
     )
 }
