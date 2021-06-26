4388ed6a2fb3802ee849dc2d52edd11236835a5e
babyaced <dannyr.simpson@gmail.com>
2021-06-26 10:12:10 -0700

    local state update for My Pets Page on pet-profile-creation

--- application/backend/routes/pet-profile.js
+++ application/backend/routes/pet-profile.js
@@ -84,7 +84,7 @@ router.post("/api/create-pet-profile", (req, res) => {
                 });
               }
               console.log("success!");
-              return res.status(200).json("success");
+              return res.status(200).json({ data: insertedProfile.insertId });
             });
           } else if (catBreed.length !== 0 && petType.label == "Cat") {
             const insertedCat = await connection
@@ -110,7 +110,7 @@ router.post("/api/create-pet-profile", (req, res) => {
                 });
               }
               console.log("success!");
-              return res.status(200).json("success");
+              return res.status(200).json({ data: insertedProfile.insertId });
             });
           } else {
             conn.commit(function (err) {
@@ -121,7 +121,12 @@ router.post("/api/create-pet-profile", (req, res) => {
                 });
               }
               console.log("success!");
-              return res.status(200).json("success");
+              return res.status(200).json({
+                display_name: name,
+                profile_id: insertedProfile.insertId,
+                profile_pic_link:
+                  "https://csc648groupproject.s3-us-west-2.amazonaws.com/DefaultProfilePic.svg",
+              });
             });
           }
         } catch (err) {
